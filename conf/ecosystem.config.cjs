b96fc2c5819d2b81f338e1717ef00f63fa629074
xdea123 <xdea123@flow.team>
2024-01-21 11:28:53 +0900

    fork 모드 변경

--- conf/ecosystem.config.cjs
+++ conf/ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: "tagall",
     script: "./app.js",
     instances: 1, // 0이면 CPU 개수에 맞춰 스레드 생성
-    exec_mode: 'cluster', // fork가 아닌 cluster 모드로 실행
+    exec_mode: 'fork', // fork가 아닌 cluster 모드로 실행
     merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
     autorestart: false, // 프로세스 실패 시 자동으로 재시작할지 선택
     watch: true, // 파일이 변경되었을 때 재시작 할지 선택
